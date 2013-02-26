9b9e3849885c87191f40df8fac5f8e49b101e04d
Richard Townsend <Richard.Townsend@warwick.ac.uk>
2013-02-26 15:11:24 +0000

    Moved functions back into index.html

--- paths.js
+++ paths.js
@@ -15,24 +15,3 @@ var waitOnQueryTestFiles = [
 	"samples/wait_normal.json",
 	"samples/wait_fulfilled.json"
 ] 
-
-function generateRPCURL(from_url) {
-	return "samples/" + from_url;
-}
-
-function checkAvailability() {
-	return apiBase;
-}
-
-function submitQuery() {
-	return apiBase+"/submit";
-	//return chooseRandomElem(submitQueryTestFiles);
-}
-
-function submitEmail() {
-	return apiBase + "/add_email"
-}
-
-function waitForQuery() {
-	return apiBase+"/status";
-}
